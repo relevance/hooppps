d190cafb819e19375b05ef51d6317907707f6016
Michael Parenteau <prmparenteau@aol.com>
2010-09-12 14:41:21 -0400

    first check in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hooppps::Application.config.secret_token = '1b27147a68c6a8cd77b1d3799435b83692ffedc30ece10eab78b369d002520c645d7b901e1a0d5bd6a7dd16b4f0616624fc9e601177c28fbe13c8b28292a8b79'
