da35956b7f20269fbd3b1a0966fe98f5aa08ea38
Michael Parenteau <prmparenteau@aol.com>
2010-09-25 18:27:01 -0400

    removed jqtouch, created new logo icon, added an about section, added some shot info. Styling still needs to happen around shot info. A show page needs to happen around the @shot.player.name and also pagination of all the 'popular shots' on the inde page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,50 +1,15 @@
-var jQT = new $.jQTouch({
-    icon: 'jqtouch.png',
-    addGlossToIcon: false,
-    startupScreen: 'jqt_startup.png',
-    statusBar: 'black',
-    preloadImages: [
-        '../../themes/jqt/img/back_button.png',
-        '../../themes/jqt/img/back_button_clicked.png',
-        '../../themes/jqt/img/button_clicked.png',
-        '../../themes/jqt/img/grayButton.png',
-        '../../themes/jqt/img/whiteButton.png',
-        '../../themes/jqt/img/loading.gif'
-        ]
-});
-// Some sample Javascript functions:
-$(function(){
-    // Show a swipe event on swipe test
-    $('#swipeme').swipe(function(evt, data) {                
-        $(this).html('You swiped <strong>' + data.direction + '</strong>!');
+// toggle content div and about div visibility
+// ------------------------------------------
+$(function() {
+  $('.about').toggle(function() {
+    $('#content').fadeOut(400, function() {
+      $('.about').text('close');
+      $('#about').slideDown(400);
     });
-    $('a[target="_blank"]').click(function() {
-        if (confirm('This link opens in a new window.')) {
-            return true;
-        } else {
-            $(this).removeClass('active');
-            return false;
-        }
+  }, function() {
+    $('#about').slideUp(400, function() {
+      $('.about').text('about');
+      $('#content').fadeIn('fast');
     });
-    // Page animation callback events
-    $('#pageevents').
-        bind('pageAnimationStart', function(e, info){ 
-            $(this).find('.info').append('Started animating ' + info.direction + '&hellip; ');
-        }).
-        bind('pageAnimationEnd', function(e, info){
-            $(this).find('.info').append(' finished animating ' + info.direction + '.<br /><br />');
-        });
-    // Page animations end with AJAX callback event, example 1 (load remote HTML only first time)
-    $('#callback').bind('pageAnimationEnd', function(e, info){
-        if (!$(this).data('loaded')) {                      // Make sure the data hasn't already been loaded (we'll set 'loaded' to true a couple lines further down)
-            $(this).append($('<div>Loading</div>').         // Append a placeholder in case the remote HTML takes its sweet time making it back
-                load('ajax.html .info', function() {        // Overwrite the "Loading" placeholder text with the remote HTML
-                    $(this).parent().data('loaded', true);  // Set the 'loaded' var to true so we know not to re-load the HTML next time the #callback div animation ends
-                }));
-        }
-    });
-    // Orientation callback event
-    $('body').bind('turn', function(e, data){
-        $('#orient').html('Orientation: ' + data.orientation);
-    });
+  });
 });
