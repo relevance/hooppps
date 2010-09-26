e63616ce3380333ead94c16c8894874db93d49a0
Michael Parenteau <prmparenteau@aol.com>
2010-09-25 20:34:54 -0400

    adding Rack::Auth::Basic until this app is further along

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "password" && password == "hooppps"
+    end
+  end
 end
