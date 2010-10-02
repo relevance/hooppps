5e4ee8c2fe6aff0c45fa07eea1ff17c33d5bfcc2
Jon Distad <jon.distad@gmail.com>
2010-10-02 17:28:07 -0400

    very, very rudimentary shot comment support

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -1,12 +1,20 @@
 class ShotsController < ApplicationController
   
   def index
-    @page = params[:page].to_i.nonzero? || 1
+    @page = real_page_number(params[:page])
     @shots = Dribbble::Shot.popular(:page => @page, :per_page => 15)
   end
   
+  
   def show
     @shot = Dribbble::Shot.find(params[:id])
+    @comments = @shot.comments(:page => real_page_number(params[:comments_page]))
   end
-  
+
+  private
+
+  def real_page_number(param)
+    param.to_i.nonzero? || 1
+  end
+
 end
