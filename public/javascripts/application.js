6c0dbcd0ee13619138e5f8148a356ece3b5aa444
Michael Parenteau <prmparenteau@aol.com>
2010-10-03 15:08:30 -0400

    reverting to the state before last commit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,24 +1,10 @@
 // hide address bar (or at least scroll below)
 window.scrollTo(0, 1); 
 
-// add class on body when orientation changes
-window.onorientationchange = function() {
-  var orientation = window.orientation;
-  switch(orientation) {
-    case 0:
-        document.body.setAttribute("class","portrait");
-        break;          
-    case 90:
-        document.body.setAttribute("class","landscape");
-        break;
-    case -90:  
-        document.body.setAttribute("class","landscape");
-        break;
-  }
-}
 // toggle content div and about div visibility
 // also toggle button text about/close
 // ------------------------------------------
+
 $(function() {
   $('.about').toggle(function() {
     $('#content').fadeOut(400, function() {
