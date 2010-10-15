49bd68090dec2f299a991c4641abafddc8f80465
Alex Redington <pair@thinkrelevance.com>
2010-10-15 12:23:39 -0400

    Adding jump to player mechanism. Requires knowing the player's login name.

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,4 +1,10 @@
 class PlayerController < ApplicationController
+
+  def index
+    if params[:id] && @player = Dribbble::Player.find(params[:id])
+      redirect_to :action => "show", :id => params[:id]
+    end
+  end
   
   def show
     @player = Dribbble::Player.find(params[:id])
