aabf8aaa91de25848784e3de8078d2f1ff50d03a
Alex Redington <pair@thinkrelevance.com>
2010-10-15 11:25:11 -0400

    Back button which uses the window.history with some javascript to do back navigation. Back button should only be rendered appropriately, e.g. users won't see it on the Hooppps index page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def back_button
+    if request.referer =~ /http:\/\/([^\/]*)\//
+      refering_host = $1
+      if request.url =~ /http:\/\/([^\/]*)\//
+        current_host = $1
+        "<a class='back_button' href='javascript:window.history.back()'>Back</a>" if refering_host == current_host
+      end
+    end
+  end
+  
 end
