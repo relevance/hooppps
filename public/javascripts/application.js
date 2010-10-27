d6c5bb187c05603c006bd44d7fbf381b1e1082b9
Larry Karnowski <larry@hickorywind.org>
2010-10-26 22:33:29 -0400

    Added bounds-checking to the iPhone scroll logic.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,6 +5,22 @@ window.scrollTo(0, 1);
 // also toggle button text about/close
 // ------------------------------------------
 
+function pixels(str) {
+  // TODO: string.split() doesn't work on IE, right?
+  return parseInt(str.split("px")[0]);
+}
+
+function newMargin(margin, delta, maxHeight) {
+  maxHeight = maxHeight * -1;
+  
+  margin = (delta > 0) ? margin+30 : margin-30;
+  margin = (margin > 0) ? 0 : margin;
+  margin = (margin < maxHeight) ? maxHeight : margin;
+  margin = margin + "px";
+  
+  return margin;
+}
+
 $(function() {
   $('.about').toggle(function() {
       $('.about').text('close');
@@ -29,15 +45,11 @@ $(function() {
   $('#track').mousewheel(function(event, delta, deltaX, deltaY) {
     event.preventDefault();
 
-    // SPIKE!!!  doesn't handle bounds-checking or scroll velocity
-    var margin = $(this).css("margin-top") || 0;
-    margin = parseInt(margin.split("px")[0]);
-    margin = (delta > 0) ? margin+10 : margin-10;
-    margin = margin + "px";
-    // console.debug(margin);
-    
-    $(this).css("margin-top", margin);
+    var margin = pixels($(this).css("margin-top")) || 0;
+    var frameHeight = pixels($("#frame").css("height"));
+    var contentHeight = pixels($("#content").css("height"));
+    var maxHeight = contentHeight - frameHeight + 100;
+
+    $(this).css("margin-top", newMargin(margin, delta, maxHeight));
   });
 });
-
-
