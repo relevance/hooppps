bc7b500b6485dc0d3cb6104a55075af180a3b476
Jon Distad <jon.distad@gmail.com>
2010-10-29 11:57:41 -0400

    Heroku-specific redis info

--- config/initializers/swish.rb
+++ config/initializers/swish.rb
@@ -1,2 +1,9 @@
-Dribbble::Config.enable_redis = true
-Dribbble::Config.expire_time = 60
+if Rails.env.production?
+  Dribbble::Config.enable_redis = true
+  Dribbble::Config.expire_time = 60
+
+  redis_info = URI.parse(ENV['REDISTOGO_URL'])
+  Dribbble::Config.redis_host = redis_info.host
+  Dribbble::Config.redis_port = redis_info.port
+  Dribbble::Config.redis_password = redis_info.password
+end
