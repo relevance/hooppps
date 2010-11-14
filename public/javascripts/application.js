8fc44a4a18bf0e091751ae4837fcd26271450a7c
Michael Parenteau <prmparenteau@aol.com>
2010-11-13 21:24:13 -0500

    removing unnecessary commentted out js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -21,15 +21,6 @@ function newMargin(margin, delta, maxHeight) {
 }
 
 $(function() {
-  // $('.about').toggle(function() {
-  //       $('.about').text('close');
-  //       $('#nav').fadeOut(400);
-  //       $('#about').fadeIn(400);
-  //     }, function() {
-  //     $('#about').slideUp(400, function() {
-  //       $('.about').text('About Hoops');
-  //     });
-  //   }),
   
   $('.back_button').click(function() {
     $('#nav').fadeOut(400, function() {
