45630babd0d2a74ee49fb4abd2eddd575826b8f2
Michael Parenteau <prmparenteau@aol.com>
2011-09-02 13:36:54 -0400

    perhaps this will fix the assets

--- config/application.rb
+++ config/application.rb
@@ -7,12 +7,7 @@ require "rails/test_unit/railtie"
 require "sprockets/railtie"
 
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  # Bundler.require *Rails.groups(:assets => %w(development test))
-  # If you want your assets lazily compiled in production, use this line
-  Bundler.require(:default, :assets, Rails.env)
-end
+Bundler.require *Rails.groups(:assets) if defined?(Bundler)
 
 module Hooppps
   class Application < Rails::Application
