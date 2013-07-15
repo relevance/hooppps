c387687efc22d5bffcc97fc0162c2343737f7799
Michael Parenteau <prmparenteau@aol.com>
2013-07-14 19:13:39 -0500

    updating tilt and using default application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,19 @@ end
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  layout :detect_browser
+  #layout :detect_browser
 
   rescue_from ActionController::RoutingError, RecordNotFound, :with => :render_not_found
-    
+
   protected
-      
+
   def render_not_found(exception)
     render :template => "errors/404", :status => 404
   end
-  
+
   private
   MOBILE_BROWSERS = ["android", "iPod", "iPhone", "opera mini", "blackberry", "palm","hiptop","avantgo","plucker", "xiino","blazer","elaine", "windows ce; ppc;", "windows ce; smartphone;","windows ce; iemobile", "up.browser","up.link","mmp","symbian","smartphone", "midp","wap","vodafone","o2","pocket","kindle", "mobile","pda","psp","treo", "maemo"]
-  
+
   def detect_browser
       agent = request.headers["HTTP_USER_AGENT"].downcase
       MOBILE_BROWSERS.each do |m|
